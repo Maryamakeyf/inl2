541a115718f9340cc233b436eec66315df47b0ae
maryamakeyf <maryamosalad98@outlook.com>
2024-04-05 21:33:23 +0200

    add other broken rules and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 // All the rules broken in the code;
 
 // 1- Comments- This code is saturated with unnecessary comments such as commenting on the imports and variable declarations.
-// Good code mostly documents itself. This is visible in lines----------------------
+// Good code mostly documents itself. This is visible in lines 19,22,27 etc
 
 //2-Formatting- In this code, the formatting is not consistent. tex line 17 is not consistent with the way other variables have been defined in the code.
 
-//3- Variables- Variables in clean code are supposed to be consistent, make sense and descriptive. These rules are broken in line 26. Val is not descriptive and not consistent with how other variables are desclared in the code.
+//3- Variables- Variables in clean code are supposed to be consistent, make sense and descriptive. These rules are broken in line 32. Val is not descriptive and not consistent with how other variables are desclared in the code.
 
 //4-Components- According to bulletproof-react code should be broken down into components rather than having one large component. To adhere to clean code principles this code should be broken down into smaller, focused components, each responsible for a specific task.
 
 //5-Error handling- Clean code states that thrown errors are good to have. If this code were to be writte correclty i would incorporate error handling mechanism like displaying error message when form submission fails.
+
+// 6- no-unused-vars- ESLint checks for variables that are declared but not used in this case i have disabled that rule.   The parameter id is passed into the function, but it is not referenced or utilized within the function's logic in line 49.
+
+//7-Func-names- The onClick handler for the delete button uses an anonymous function expression instead of a named one, violating the func-names rule.
 
 // Imports needed for the project
 import React, { useState } from "react";
@@ -50,7 +54,7 @@ export default function Home() {
   return (
     <>
       <div>
-        <h1>To-Do List</h1>
+        <h1>To-Do APP</h1>
         <form onSubmit={handleSubmit}>
           <input
             type="text"
@@ -66,7 +70,14 @@ export default function Home() {
               {todo.text}
               {/* eslint-disable-next-line jsx-a11y/interactive-supports-focus,jsx-a11y/
               click-events-have-key-events */}
-              <button onClick={() => handleDelete(todo.id)}>Delete</button>
+              {/* Breaking the 'func-names' rule by using an anonymous function */}
+              <button
+                onClick={function () {
+                  handleDelete(todo.id);
+                }}
+              >
+                Delete
+              </button>
             </li>
           ))}
         </ul>
